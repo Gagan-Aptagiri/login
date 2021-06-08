6c072e5ee7b1e54fdcdf425b7a6d1786350b93e2
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-06-08 22:16:56 +0530

    MySQL database connected

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
 const port = 5000;
+
+const db = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'login',
+});
+
+db.connect((error) => {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log('MySQL Connected.');
+	}
+});
 
 app.get('/', (req, res) => {
 	res.send('<h1>Hello From Node.js</h1>');
